9025c9d240ac7beebf0e53ff55eb56351d4228ed
itzysmeyash <itzysmeyash@gmail.com>
2026-03-20 17:36:03 +0530

    library.sql

--- library.sql
+++ library.sql
@@ -1,13 +1,19 @@
-CREATE DATABASE library;
-
-USE library;
-
 CREATE TABLE books (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    author VARCHAR(100),
+    book_id INT PRIMARY KEY,
+    title VARCHAR(50),
+    author VARCHAR(50),
     status VARCHAR(20)
 );
 
-INSERT INTO books (name, author, status)
-VALUES ('C++ Basics', 'Bjarne', 'Available');
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE issue (
+    issue_id INT PRIMARY KEY,
+    book_id INT,
+    student_id INT,
+    issue_date DATE,
+    return_date DATE
+);
