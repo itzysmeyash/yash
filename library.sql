89a6cea8026dbc9d9e839e9e81abfcd493fa18e1
itzysmeyash <itzysmeyash@gmail.com>
2026-03-20 23:56:31 +0530

    library.sql

--- library.sql
+++ library.sql
@@ -1,40 +1,24 @@
-CREATE DATABASE library_db;
+CREATE DATABASE library;
+USE library;
 
-USE library_db;
-
--- Users Table
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    role ENUM('admin','student') NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    role VARCHAR(10)
 );
 
--- Books Table
 CREATE TABLE books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100),
-    author VARCHAR(100),
-    status VARCHAR(20) DEFAULT 'available'
+    author VARCHAR(50),
+    status VARCHAR(20)
 );
 
--- Transactions Table
 CREATE TABLE transactions (
-    trans_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     book_id INT,
     issue_date DATE,
-    return_date DATE,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    return_date DATE
 );
-
--- Sample Data
-INSERT INTO users (username, password, role)
-VALUES ('admin', '1234', 'admin'),
-       ('student1', '1234', 'student');
-
-INSERT INTO books (title, author)
-VALUES ('C Programming', 'Dennis Ritchie'),
-       ('Database Systems', 'Elmasri'),
-       ('Web Development', 'MDN');
