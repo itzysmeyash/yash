8b54f9a9d16cca804241af9f115d14a6dfc01da8
itzysmeyash <itzysmeyash@gmail.com>
2026-03-20 23:55:38 +0530

    library.cpp

--- library.cpp
+++ library.cpp
@@ -1,61 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Library {
 public:
-    void addBook() {
-        string title;
-        cout << "Enter Book Title: ";
-        cin >> title;
-        cout << "Book Added Successfully!\n";
-    }
-
-    void deleteBook() {
-        int id;
-        cout << "Enter Book ID to Delete: ";
-        cin >> id;
-        cout << "Book Deleted Successfully!\n";
-    }
-
-    void issueBook() {
-        int id;
-        cout << "Enter Book ID to Issue: ";
-        cin >> id;
-        cout << "Book Issued Successfully!\n";
-    }
-
-    void returnBook() {
-        int id;
-        cout << "Enter Book ID to Return: ";
-        cin >> id;
-        cout << "Book Returned Successfully!\n";
-    }
+    void addBook() { cout<<"Book Added\n"; }
+    void deleteBook() { cout<<"Book Deleted\n"; }
+    void issueBook() { cout<<"Book Issued\n"; }
+    void returnBook() { cout<<"Book Returned\n"; }
 };
 
 int main() {
-    Library lib;
-    int choice;
+    Library l;
+    int ch;
 
-    do {
-        cout << "\n===== Library Menu =====\n";
-        cout << "1. Add Book\n";
-        cout << "2. Delete Book\n";
-        cout << "3. Issue Book\n";
-        cout << "4. Return Book\n";
-        cout << "5. Exit\n";
-        cout << "Enter choice: ";
-        cin >> choice;
+    cout<<"1 Add\n2 Delete\n3 Issue\n4 Return\n";
+    cin>>ch;
 
-        switch(choice) {
-            case 1: lib.addBook(); break;
-            case 2: lib.deleteBook(); break;
-            case 3: lib.issueBook(); break;
-            case 4: lib.returnBook(); break;
-            case 5: cout << "Exiting...\n"; break;
-            default: cout << "Invalid choice!\n";
-        }
-
-    } while(choice != 5);
-
-    return 0;
+    switch(ch) {
+        case 1: l.addBook(); break;
+        case 2: l.deleteBook(); break;
+        case 3: l.issueBook(); break;
+        case 4: l.returnBook(); break;
+        default: cout<<"Invalid";
+    }
 }
